0e06d0a139785fc26decd137556d7c9e1aa14889
Ruler90 <54283970+Ruler90@users.noreply.github.com>
2019-12-01 23:12:17 +0100

    change quotes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component} from "react";
-import "./scss/App.css";
+import React, { Component} from 'react';
+import './scss/App.css';
 
 export default class App extends Component{
   render(){
